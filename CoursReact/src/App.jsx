90e8dc08a50d0c0764f4278c607093917fa21eef
Ryan Sifi <ryan.sifi.77@gmail.com>
2024-12-17 10:33:21 +0100

    first commit

---
+++ CoursReact/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Footer from './assets/composants/containers/Footer'
+
+function App() {
+
+  return (
+    <>
+      <h1>Test</h1>
+      <Footer />
+    </>
+  )
+}
+export default App
---
+++ CoursReact/src/assets/composants/containers/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer style={styles.footer}>
+      <p>© 2024 Mon Application. Tous droits réservés.</p>
+    </footer>
+  );
+};
+
+const styles = {
+  footer: {
+    backgroundColor: "#282c34",
+    color: "white",
+    textAlign: "center",
+  },
+};
+
+export default Footer;
