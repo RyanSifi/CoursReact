f2a01a5195cc7b6c9e9dfe7c58cf0776e42157b6
Ryan Sifi <ryan.sifi.77@gmail.com>
2024-12-18 16:31:49 +0100

    Exo Add

--- CoursReact/src/composants/Menu.jsx
+++ CoursReact/src/composants/Menu.jsx
@@ -1,10 +1,27 @@
 import { useState } from "react"
+import Hobbies from "./Hobbies";
 
 const Menu = () => {
   const [isMenuVisible, setMenuVisible] = useState(false);
+  const [hobbies, setHobbies] = useState([
+    "Nourrir le chien",
+    "Faire ses devoirs",
+    "Essayer des trucs en React",
+  ]);
+  const [newHobby, setNewHobby] = useState("");
 
   const toggleMenu = () => {
     setMenuVisible(!isMenuVisible);
+  };
+
+  const addHobby = () => {
+    if (newHobby.trim() === "") {
+      alert("Le champ ne peut pas être vide !");
+      return;
+    }
+
+    setHobbies([...hobbies, newHobby.trim()]); 
+    setNewHobby(""); 
   };
 
   return (
@@ -13,11 +30,21 @@ const Menu = () => {
         {isMenuVisible ? 'Masquer' : 'Afficher'}
       </button>
       {isMenuVisible && (
-        <ul style={styles.menu}>
-          <li>Nourrir le chien</li>
-          <li>Faire ses devoir</li>
-          <li>Essayer des truc en React</li>
-        </ul>
+        <div style={styles.menu}>
+          <Hobbies hobbies={hobbies} />
+          <div style={styles.inputContainer}>
+            <input
+              type="text"
+              placeholder="Ajout hobby"
+              value={newHobby}
+              onChange={(e) => setNewHobby(e.target.value)}
+              style={styles.input}
+            />
+            <button onClick={addHobby} style={styles.addButton}>
+              Ajouter
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
